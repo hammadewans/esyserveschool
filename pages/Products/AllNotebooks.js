5657ea916656faafd97d90de0fa1975ab6f86175
hammadewans <ewans.hammad@gmail.com>
2026-04-06 11:15:45 +0530

    Delete pages directory

--- pages/Products/AllNotebooks.js
+++
@@ -1,9 +0,0 @@
-import ProductGridTemplate from '/components/ProductGridTemplate.js';
-import { notebookDatabase } from '/assets/js/data/notebook-database.js';
-
-export default function ProductsNotebook() {
-    ProductGridTemplate(
-        notebookDatabase.notebooks,
-        '/notebook-details'
-    );
-}
--- pages/Products/AllPolybags.js
+++
@@ -1,9 +0,0 @@
-import ProductGridTemplate from '/components/ProductGridTemplate.js';
-import { polybagDatabase } from '/assets/js/data/polybag-database.js';
-
-export default function ProductsPolybag() {
-    ProductGridTemplate(
-        polybagDatabase.polybags,
-        '/polybag-details'
-    );
-}
