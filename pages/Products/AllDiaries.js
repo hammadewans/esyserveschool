d2bef99b651a8742d945702109c9c4a92d7eb8c6
hammadewans <ewans.hammad@gmail.com>
2026-09-02 14:40:44 +0530

    Delete pages directory

--- pages/Products/AllDiaries.js
+++
@@ -1,9 +0,0 @@
-import ProductGridTemplate from '/components/ProductGridTemplate.js';
-import { diaryDatabase } from '/assets/js/data/diary-database.js';
-
-export default function ProductsDiary() {
-    ProductGridTemplate(
-        diaryDatabase.diaries,
-        '/diary-details'
-    );
-}
