fe146a35546c2cd2d15f6fde46cfcf2aa1bace0c
hammadewans <ewans.hammad@gmail.com>
2026-09-02 16:07:10 +0530

    Delete pages directory

--- pages/Products/AllCards.js
+++
@@ -1,9 +0,0 @@
-import ProductGridTemplate from '/components/ProductGridTemplate.js';
-import { cardsDatabase } from '/assets/js/data/cards-database.js';
-
-export default function ProductsIdCard() {
-    ProductGridTemplate(
-        cardsDatabase.idCards,
-        '/card-details'
-    );
-}
